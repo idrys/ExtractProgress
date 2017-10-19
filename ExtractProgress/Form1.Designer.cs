49d36eedb476887fb388b238aa3292dff0a186c7
idrys <idrys2@gmail.com>
2017-10-19 11:11:31 +0200

    Postęp rozpakowania 7z

--- ExtractProgress/Form1.Designer.cs
+++ ExtractProgress/Form1.Designer.cs
@@ -28,12 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnExtract = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnExtract
+            // 
+            this.btnExtract.Location = new System.Drawing.Point(88, 145);
+            this.btnExtract.Name = "btnExtract";
+            this.btnExtract.Size = new System.Drawing.Size(75, 23);
+            this.btnExtract.TabIndex = 0;
+            this.btnExtract.Text = "Rozpakuj";
+            this.btnExtract.UseVisualStyleBackColor = true;
+            this.btnExtract.Click += new System.EventHandler(this.btnExtract_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(63, 58);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(132, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnExtract);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnExtract;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- ExtractProgress/Form1.cs
+++ ExtractProgress/Form1.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,5 +18,65 @@ namespace ExtractProgress
         {
             InitializeComponent();
         }
+
+        private void btnExtract_Click(object sender, EventArgs e)
+        {
+
+            Task.Run(() =>
+
+                SevenZipExtractProgress(@"c:\Users\Slawek\Repo\plytki_cersanit_06_2017.exe", @"C:\CADProjekt\CAD Decor Paradyz v. 2.3.0\", onProgres)
+            );
+        }
+
+        private void onProgres(int uploaded)
+        {
+            Debug.WriteLine("Postęp: " + uploaded);
+            progressBar1.Invoke((MethodInvoker)delegate { progressBar1.Value = (int)uploaded; });
+        }
+
+        public bool SevenZipExtractProgress(string pathFile, string folderExtract, Action<int> onProgress)
+        {
+            Regex REX_SevenZipStatus = new Regex(@"(?<p>[0-9]+)%");
+
+            int EverythingOK = -1;
+            string testInfo = string.Empty;
+            string path7zip = "x86\\7z.exe";
+
+            if (Environment.Is64BitOperatingSystem)
+                path7zip = "x64\\7z.exe";
+
+            
+            Process p = new Process();
+            p.StartInfo.FileName = path7zip;
+            p.StartInfo.Arguments = "e " + "\"" + pathFile + "\"" + " -o\"" + folderExtract + "\"" + " -y -bsp1 -bse1 -bso1";
+            p.StartInfo.UseShellExecute = false;    // Nie zbędne do odczytu wartości z wyjścia 7z
+            p.StartInfo.RedirectStandardOutput = true;  // Nie zbędne do odczytu wartości z wyjścia 7z
+
+            p.OutputDataReceived += (sender, e) => {    // Odczyt procentów z wyjścia 7z
+                Debug.WriteLine("onProgress: " + onProgress.ToString());
+                if (onProgress != null)
+                {
+                    Match m = REX_SevenZipStatus.Match(e.Data ?? "");
+                    if (m != null && m.Success)
+                    {
+                        int procent = int.Parse(m.Groups["p"].Value);
+                        //progressBar1.Invoke((MethodInvoker)delegate { progressBar1.Maximum = 100; });
+                        Debug.WriteLine("Procenty: " + procent);
+                        onProgress(procent); // delegat link do metody                        
+                    }
+                }
+            };
+
+            p.StartInfo.CreateNoWindow = true; // Ukrycie okna
+
+            p.Start();
+            p.BeginOutputReadLine(); // Nie zbędne do odczytu wartości z wyjścia 7z
+            p.WaitForExit();
+            p.Close();
+
+            EverythingOK = testInfo.IndexOf("Everything is Ok");
+            return EverythingOK == -1 ? false : true;
+        }
+
     }
 }
